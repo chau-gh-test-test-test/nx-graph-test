073e488534bc8df84d70c221f63bcf6e22201f9a
nartc <nartc7789@gmail.com>
2024-11-02 13:54:59 -0500

    add icon

--- libs/forms/input/src/lib/input.tsx
+++ libs/forms/input/src/lib/input.tsx
@@ -1,7 +1,10 @@
+import { Icon } from '@nx-graph-test/icon';
+
 export function Input() {
   return (
     <div className="text-pink-500">
       <h1>Welcome to Input!</h1>
+      <Icon />
     </div>
   );
 }
---
+++ libs/icon/src/lib/icon.tsx
@@ -0,0 +1,9 @@
+export function Icon() {
+  return (
+    <div className="text-pink-500">
+      <h1>Welcome to Icon!</h1>
+    </div>
+  );
+}
+
+export default Icon;
