bf1195a96a0f076ae4eb840cd93e8bcdcdd31221
nartc <nartc7789@gmail.com>
2024-11-05 14:58:37 -0600

    add zod

--- libs/icon/src/lib/icon.tsx
+++ libs/icon/src/lib/icon.tsx
@@ -1,4 +1,10 @@
+import { z } from 'zod';
+
+const schema = z.object({ hi: z.string() });
+
 export function Icon() {
+  console.log(schema);
+
   return (
     <div className="text-pink-500">
       <h1>Welcome to Icon!</h1>
