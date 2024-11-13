00cde0f46ff267088749718b5013ab87048b3175
nartc <nartc7789@gmail.com>
2024-11-13 14:24:06 -0600

    touch input

--- libs/forms/input/src/lib/input.tsx
+++ libs/forms/input/src/lib/input.tsx
@@ -3,7 +3,7 @@ import { Icon } from '@nx-graph-test/icon';
 export function Input() {
   return (
     <div className="text-pink-500">
-      <h1>Welcome to Input!</h1>
+      <h1>Welcome to Input!!!</h1>
       <Icon />
     </div>
   );
